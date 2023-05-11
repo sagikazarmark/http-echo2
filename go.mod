368ac3e69e604048638f81c4db24ea40d7ef5d98
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-05-10 22:43:49 -0700

    feat: get the value of a single environment variable
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sagikazarmark/http-echo2
 
 go 1.19
+
+require github.com/go-chi/chi/v5 v5.0.8 // indirect
--- main.go
+++ main.go
@@ -4,23 +4,34 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	router := http.NewServeMux()
+	router := chi.NewRouter()
 
 	hello := os.Getenv("HELLO")
 	if hello == "" {
 		hello = "[unknown]"
 	}
 
-	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello %s!", hello)
 	})
 
-	router.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
+	router.Get("/env", func(w http.ResponseWriter, r *http.Request) {
 		for _, env := range os.Environ() {
 			fmt.Fprintln(w, env)
+		}
+	})
+
+	router.Get("/env/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+
+		value, ok := os.LookupEnv(key)
+		if ok {
+			fmt.Fprint(w, value)
 		}
 	})
 
